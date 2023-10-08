3ec3ecb6ee2aeae6c69b6d3b0aa8def57b758011
Mustafa ÇELİK <mustafacelik0698@gmail.com>
2023-10-09 01:02:27 +0300

    assignment

--- Controllers/AssignmentController.cs
+++ Controllers/AssignmentController.cs
@@ -27,15 +27,12 @@ namespace OnlineLearningPlatformGroup5.Controllers
         [Authorize(Roles = SD.Role_Admin + ","+ SD.Role_Instructor)]
         public IActionResult Create(int id)
         {
-<<<<<<< HEAD
             ViewBag.Id = id;    
             //Course course = _contextAssignment.Course.FirstOrDefault(x => x.Id == id);
             //ViewBag.Course = course;
-=======
             
             Course course = _contextAssignment.Course.FirstOrDefault(x => x.Id == id);
             ViewBag.Course = course;
->>>>>>> 69396fe60a98ac123aff8c08447747050af3017f
             return View();
         }
 
@@ -43,7 +40,6 @@ namespace OnlineLearningPlatformGroup5.Controllers
         [HttpPost]
         public IActionResult Create([Bind(include:"Id,CourseId,Title,Description,DueDate")] Assignment assignment)
         {
-<<<<<<< HEAD
             //_contextAssignment.Database.ExecuteSqlRaw
             using (var transaction = _contextAssignment.Database.BeginTransaction())
             {
@@ -57,13 +53,11 @@ namespace OnlineLearningPlatformGroup5.Controllers
             //_contextAssignment.Assignment.Add(assignment);
             //_contextAssignment.SaveChanges();
             return RedirectToAction("ListCourses");
-=======
 
             _contextAssignment.Assignment.Add(assignment);
             _contextAssignment.SaveChanges();
 
             return Redirect("ListCourses");
->>>>>>> 69396fe60a98ac123aff8c08447747050af3017f
         }
 
     }
